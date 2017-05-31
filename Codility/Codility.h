a250f0170e72a171f75b1ef4ec6c8c5244e5ec11
seojingyo <seojingyo@gmail.com>
2017-05-31 14:11:27 +0900

    * Project Import

---
+++ Codility/Codility.h
@@ -0,0 +1,19 @@
+//
+//  Codility.h
+//  Codility
+//
+//  Created by 122316 on 2017. 5. 31..
+//  Copyright © 2017년 seojingyo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Codility.
+FOUNDATION_EXPORT double CodilityVersionNumber;
+
+//! Project version string for Codility.
+FOUNDATION_EXPORT const unsigned char CodilityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Codility/PublicHeader.h>
+
+
