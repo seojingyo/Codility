aadcade792da1f310751ce0f34927a56d9999809
seojingyo <seojingyo@gmail.com>
2017-06-07 14:00:03 +0900

    * Lession 4 (1/4) update

---
+++ Codility/FrogRiverOne.h
@@ -0,0 +1,15 @@
+//
+//  FrogRiverOne.h
+//  Codility
+//
+//  Created by 122316 on 2017. 6. 2..
+//  Copyright © 2017년 seojingyo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FrogRiverOne : NSObject
+
++(int)solution:(int)X Array:(NSMutableArray *)A;
+
+@end
