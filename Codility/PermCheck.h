cb42ed00952e3b8117472babec3afbe4219fcb4a
seojingyo <seojingyo@gmail.com>
2017-06-08 13:29:31 +0900

    * Lesssion 2/4

---
+++ Codility/PermCheck.h
@@ -0,0 +1,15 @@
+//
+//  PermCheck.h
+//  Codility
+//
+//  Created by 122316 on 2017. 6. 2..
+//  Copyright © 2017년 seojingyo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PermCheck : NSObject
+
++(int)solution:(NSMutableArray *)A;
+
+@end
